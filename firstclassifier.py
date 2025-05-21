51cb75bb872275a25e662ba724eaad5149d488fb
oparker7 <oliverrhysparker@gmail.com>
2025-05-21 18:10:48 +0100

    first classifier progress

--- firstclassifier.py
+++ firstclassifier.py
@@ -1,6 +1,7 @@
 import numpy as np
+from scipy.ndimage import gaussian_filter1d
 
-def fanofac(spikes, bin_width, T=1000):
+def fanoFac(spikes, bin_width, T=1000):
     edges = np.arange(0, T + 1, bin_width)
     
     binned = np.add.reduceat(spikes, edges[:-1], axis=1)
@@ -15,3 +16,53 @@ def fanofac(spikes, bin_width, T=1000):
     times = edges[:-1] + bin_width / 2
     
     return times, fano
+
+def genFanos(dataset, datasize, T=1000, bw=20):
+
+    fanos = np.array([fanoFac(dataset[i],
+                    bin_width=bw,
+                    T=T)[1]
+                    for i in range(datasize*2)]
+                )
+
+    n = len(fanos[0])
+    clip_fac = 0.1 # fraction of array to remove from start and end
+    fanos = np.array(fanos[:, int(np.floor(n*clip_fac)) : int(np.ceil(n*(1-clip_fac)))])
+    fanos = gaussian_filter1d(fanos, sigma=1, axis=1)
+    fanos_clean = np.nan_to_num(fanos, nan=0.0)
+
+    return fanos_clean
+
+def maxFanoFac(fanos, threshold=1.22):
+
+    f_max = np.max(fanos, axis=1)
+    classifier = (f_max > threshold).astype(int)
+
+    return classifier
+
+# notes: genFanoClassifyMax generates the fano factor arrays
+# and then classifies based on the maximum for each model
+# it would be nice to split into genFanos and maxFanoFac 
+# so that the fano factors are generated and can then be used for different
+# analyses but the function only seems to work when as one - have a look at ths later
+def genFanoClassifyMax(dataset, datasize, T=1000, bw=20, threshold=1.22):
+
+    fanos = np.array([fanoFac(dataset[i],
+                    bin_width=bw,
+                    T=T)[1]
+                    for i in range(datasize*2)]
+                )
+
+    n = len(fanos[0])
+    clip_fac = 0.1 # fraction of array to remove from start and end
+    fanos = np.array(fanos[:, int(np.floor(n*clip_fac)) : int(np.ceil(n*(1-clip_fac)))])
+    fanos = gaussian_filter1d(fanos, sigma=1, axis=1)
+    f_max = np.max(fanos, axis=1)
+    classifier = (f_max > threshold).astype(int)
+
+    return classifier
+
+def maxFanoDeriv(dataset, datasize,T=1000):
+    return    
+
+
